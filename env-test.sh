f9a4f5dc5a7598dd36b7ce7026145a6b4a76d5ce
André Pinela <sheffzor@gmail.com>
2017-01-22 22:13:47 +0000

    Update test environment variables
    Change-Id: I04158fc8f96ce2ec398e61d40eb025fda3c63b2a
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- env-test.sh
+++ env-test.sh
@@ -1,4 +1,15 @@
+# To test the build environment build_config project should be cloned to the same directory
+# where UA "source" is in, enter in build_config directory, edit env-test.sh and execute:
+# . env-test.sh
+# bash -xe build.sh
+
 export WORKSPACE=$(cd ..; pwd)
-export BRANCH="aosp-7.0"
+export BRANCH="aosp-7.1"
 export BUILD_TYPE="eng"
+export BUILD_PRODUCT="aosp"
 export DEVICE="arm"
+export BUILD_TARGETS=""
+export CLEAN=false
+export CLEAN_TARGETS="installclean"
+export GERRIT_CHANGES=""
+export EXTRA_DEBUGGABLE_BOOT=false
