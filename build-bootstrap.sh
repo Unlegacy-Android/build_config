a86a2625748a3b7c7c75ca96b755840d65529c67
André Pinela <sheffzor@gmail.com>
2017-01-31 21:48:11 +0000

    Import build starter script from jenkins
    Change-Id: Ia9f6479e3701e37cade15f52ce0d1a5e3124e623
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ build-bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+JENKINS_ANDROID_JOB="http://jenkins.unlegacy-android.org/job/android/buildWithParameters?token=$JENKINS_BUILD_TOKEN&IGNORE_MADE_CHANGES=false&PUBLISH_BUILD=true"
+
+WEEK_DAY_BUILD="1"
+WEEK_DAY=$(date +%u)    # day of week (1..7); 1 is Monday
+MONTH_DAY_BUILD="01"
+MONTH_DAY=$(date +%d)
+
+curl -s https://raw.githubusercontent.com/Unlegacy-Android/build_config/master/build-targets > build-targets
+
+function process_line {
+  LINE=$1
+  if [ "$LINE" != "" ] && [[ $LINE != \#* ]] ;
+    then
+      IFS=' '
+      declare -a LINE_ARRAY=($*)
+      DEVICE=${LINE_ARRAY[0]}
+      BUILD_TYPE=${LINE_ARRAY[1]}
+      BRANCH=${LINE_ARRAY[2]}
+      FREQ=${LINE_ARRAY[3]}
+      if [ "$FREQ" == "" ] || [ "$FREQ" == "D" ] ; then
+        curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
+      elif [ "$FREQ" == "W" ] && [ "$WEEK_DAY" == "$WEEK_DAY_BUILD" ] ; then
+        curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
+      elif [ "$FREQ" == "M" ] && [ "$MONTH_DAY" == "$MONTH_DAY_BUILD" ] ; then
+        curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
+      fi
+  fi
+}
+
+while read i;
+  do
+    process_line "$i";
+    sleep 1
+done < ./build-targets
