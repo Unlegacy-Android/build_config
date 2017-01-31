3bf3b2dd8d3de07401db7eb3a1a9531702a1fd8d
André Pinela <sheffzor@gmail.com>
2017-01-31 21:57:43 +0000

    Set JDK based on branch if CHECK_JDK is true
    Change-Id: I2928ff9a46c9f9d5c709572fdc43f5aeeeb32588
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- job.sh
+++ job.sh
@@ -1,3 +1,29 @@
+#!/usr/bin/env bash
+
+# This should be copied to a shell command in jenkins project
+# curl -ksO https://raw.githubusercontent.com/Unlegacy-Android/build_config/master/job.sh
+# chmod a+x job.sh
+# bash job.sh
+
+if [ -z "$CHECK_JDK" ];
+then
+  export CHECK_JDK="false"
+fi
+
+if [ "$CHECK_JDK" == "true" ]; then
+  if [ "$BRANCH" == "aosp-4.4" ]; then
+    echo "Setting default jdk to 1.7"
+    echo 1 | sudo /usr/bin/update-alternatives --config java > /dev/null
+    echo 1 | sudo /usr/bin/update-alternatives --config javac > /dev/null
+    echo 1 | sudo /usr/bin/update-alternatives --config javap > /dev/null
+  else
+    echo "Setting default jdk to 1.8"
+    echo 0 | sudo /usr/bin/update-alternatives --config java > /dev/null
+    echo 0 | sudo /usr/bin/update-alternatives --config javac > /dev/null
+    echo 0 | sudo /usr/bin/update-alternatives --config javap > /dev/null
+  fi
+fi
+
 if [ ! -d build_config ]
 then
   git clone git://github.com/Unlegacy-Android/build_config.git
