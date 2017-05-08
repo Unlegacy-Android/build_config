48dfae9cb569205ebfeb764c161af9327b097f2c
André Pinela <sheffzor@gmail.com>
2017-05-08 14:19:08 +0100

    builds trigger: give a chance to specify a build day fo weeklies and monthlies
    Change-Id: Ib6fd149302092fc9bc8773078029edf3bcb1b6aa
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- build-bootstrap.sh
+++ build-bootstrap.sh
@@ -3,9 +3,7 @@
 
 JENKINS_ANDROID_JOB="$JENKINS_URL/job/android/buildWithParameters?token=$JENKINS_BUILD_TOKEN&IGNORE_MADE_CHANGES=false&PUBLISH_BUILD=true"
 
-WEEK_DAY_BUILD="1"
 WEEK_DAY=$(date +%u)    # day of week (1..7); 1 is Monday
-MONTH_DAY_BUILD="01"
 MONTH_DAY=$(date +%d)
 
 curl -s https://raw.githubusercontent.com/Unlegacy-Android/build_config/master/build-targets > build-targets
@@ -20,11 +18,12 @@ function process_line {
       BUILD_TYPE=${LINE_ARRAY[1]}
       BRANCH=${LINE_ARRAY[2]}
       FREQ=${LINE_ARRAY[3]}
+      BUILD_DAY=${LINE_ARRAY[4]}
       if [ "$FREQ" == "" ] || [ "$FREQ" == "D" ] ; then
         curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
-      elif [ "$FREQ" == "W" ] && [ "$WEEK_DAY" == "$WEEK_DAY_BUILD" ] ; then
+      elif [ "$FREQ" == "W" ] && [ "$WEEK_DAY" == "$BUILD_DAY" ] ; then
         curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
-      elif [ "$FREQ" == "M" ] && [ "$MONTH_DAY" == "$MONTH_DAY_BUILD" ] ; then
+      elif [ "$FREQ" == "M" ] && [ "$MONTH_DAY" == "$BUILD_DAY" ] ; then
         curl $JENKINS_ANDROID_JOB\&BRANCH=$BRANCH\&DEVICE=$DEVICE\&BUILD_TYPE=$BUILD_TYPE
       fi
   fi
